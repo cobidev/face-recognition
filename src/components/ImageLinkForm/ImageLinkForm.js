7accb225724ae25507df37aa950806dc2d77c9d9
Jacobo Martinez <jacobojavier98@gmail.com>
2019-04-05 22:49:43 -0400

    change font size and with for the imageLinkForm

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -8,8 +8,8 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
         {'This Magic Brain will detect faces in your pictures. Give it a try!'}
       </p>
       <div className='flex justify-center'>
-        <div className='bg-pattern pa4 br3 shadow-5'>
-          <input onChange={onInputChange} className='w-70 f4 pa2' type="text" />
+        <div className='w-50 bg-pattern pa4 br3 shadow-5'>
+          <input onChange={onInputChange} className='w-70 f5 pa2' type="text" />
           <button onClick={onButtonSubmit} className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple pointer'>Detect</button>
         </div>
       </div>
